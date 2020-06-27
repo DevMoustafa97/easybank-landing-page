e60fd9b0c7048258a3917fbaae2ab0b25e162695
devmoustafa97 <devmoustafa97@gmail.com>
2020-06-28 01:08:36 +0200

    Initial all responsive

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+let menuBtn = document.querySelector('#menu-btn');
+let mobileNav = document.querySelector('#mobile-nav');
+
+menuBtn.addEventListener('click',()=>{
+    if(menuBtn.innerHTML != '✕'){
+
+        mobileNav.style.display = 'flex';
+        menuBtn.innerHTML = "✕"
+    }else {
+        mobileNav.style.display = 'none'
+        menuBtn.innerHTML = "|||"
+    }
+
+})
